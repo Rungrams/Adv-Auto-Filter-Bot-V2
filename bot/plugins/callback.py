ec47af76872dc999c879e561d08e1f830c2a6bd1
RUNGRAM <87849536+Rungrams@users.noreply.github.com>
2021-08-14 12:01:54 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1643,7 +1643,7 @@ async def callback_data(bot, update: CallbackQuery):
             InlineKeyboardButton('GROUP👥', url ='https://t.me/tobotupdate')
         ],[
             InlineKeyboardButton('REPO🤖', url='https://t.me/bot_repo')
-            Inlinekeyboardbutton('DEVLOAPER👩‍💻', url='https://t.me/rungram')
+            InlinekeyboardButton('DEVLOAPER👩‍💻', url='https://t.me/rungram')
         ],[
             InlineKeyboardButton('🔔SUBSCRIBE🔔', url='https://youtube.com/channel/UC1SeMwgyNDi_n4T8cbgVjKQ')
         ]]
