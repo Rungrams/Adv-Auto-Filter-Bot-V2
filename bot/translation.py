ee6b9c862c517c53958d1d161f5fd3c6a6a9a6fa
RUNGRAM <87849536+Rungrams@users.noreply.github.com>
2021-08-14 10:13:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me To Your Group And Channel And Connect Them And See My Mangic🧛‍♂️
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For More Details Click /Help Button Below..
+@tobot_update
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>👩‍💻 DEVLOAPER</b> : <b><i><a href="https://t.me/rungram">RUNGRAM</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>📢 CHANNEL</b> : <i><a href="https://t.me/tobot_update">Click Me</a></i>
 """
